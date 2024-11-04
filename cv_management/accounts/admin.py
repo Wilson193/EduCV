1aca3dffd13bdf100297270c76b4266c25955af0
oscarlp1 <oscarlozanoposso20@gmail.com>
2024-11-04 17:59:22 -0500

    Configuración para el correo institucional

--- cv_management/accounts/admin.py
+++ cv_management/accounts/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# usuarios/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('email',)
+
+admin.site.register(User, UserAdmin)
--- cv_management/accounts/views.py
+++ cv_management/accounts/views.py
@@ -4,23 +4,24 @@ from django.contrib import messages
 from docente.models import Docente  # Asegúrate de importar tu modelo
 
 def signin(request):
-    if request.method == 'POST':
-        cedula = request.POST.get('cedula')
-        password = request.POST.get('password')
-
-        try:
-            print("mostrar datos")
-            print(user = Docente.objects.get(cedula=cedula)) # Buscar por cédula
-        except Docente.DoesNotExist:
-            user = None
-
-        if user and user.clave == password:  # Comparar la contraseña
-            login(request, user)  # Iniciar sesión
-            return redirect('nombre_de_la_vista_deseada')
-        else:
-            print(request, 'Credenciales inválidas.')
-
-    return render(request, 'signin.html')
+        docente = Docente(
+        cedula=123,
+        nombre='nombre',
+        apellido='apellido',
+        num_telefono=1234,
+        universidad='universidad',
+        cargo='cargo',
+        correo='correo',
+        clave='2312',  # Si decides mantener este campo, considera encriptarlo.
+        facultad='facultad',
+        categoria='categoria',
+        especialidad='especialidad',
+        tipo_contrato='tipo_contrato',
+        estado='estado',
+        fecha_contratacion='2000-09-23'
+        )
+        docente.save()  # Guarda el nuevo docente en la base de datos
+        return render(request, 'signin.html')
 
 def signup(request):
     return render(request, 'signup.html')
